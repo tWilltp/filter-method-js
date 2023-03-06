cdec71d0c6c93dfc6504d47c24e8b5e0a165f84d
tWilltp <williamjenkinsfeb@gmail.com>
2023-03-06 15:57:21 +0000

    filte method js

--- filter.js
+++ filter.js
@@ -21,6 +21,20 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age > 21);
+console.log(oldEnough);
+
+// obtain object array
+
+const paul = people.filter(p => p.name === 'Paul');
+console.log(paul);
+
+// obtain object itself
+
+const paul2 = people.filter(p => p.name === 'Paul')[0];
+console.log(paul2);
+
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +69,48 @@ const students = [
     ]
   },
 ];
+
+// 1 function most condensed
+
+const candidates1 = students.filter(student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+  return strongSkills.length > 0;
+});
+
+console.log(candidates1);
+
+// 2 has strong skills in seperate function, called in candidates
+
+const hasStrongSkills2 = student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+  return strongSkills.length > 0;
+};
+
+const candidates2 = students.filter(hasStrongSkills2);
+
+console.log(candidates2);
+
+// 3 has strong skills in seperate function, called in candidates. has 5 years experience in seperate function, called in has strong skills
+
+const has5YrsExp = skill => skill.yrsExperience >= 5;
+
+const hasStrongSkills3 = student => {
+  let strongSkills = student.skills.filter(has5YrsExp);
+  return strongSkills.length > 0;
+};
+
+const candidates3 = students.filter(hasStrongSkills3);
+
+console.log(candidates3);
+
+// 4 /\/\ this means you can remove the return statement and the stong skills variable. just attach .length on to the inner filter === single line code
+
+const has5YrsExp2 = skill => skill.yrsExperience >= 5;
+
+const hasStrongSkills4 = student => student.skills.filter(has5YrsExp).length > 0; // << .length on inner filter
+
+const candidates4 = students.filter(hasStrongSkills3);
+
+console.log(candidates4);
+
+// /\/\ write out function in its entirety (1) then simplify
